e852d6ed74ea6b812a4d72395a7215712fdbff7a
illysito <illyalukyanovdesign@gmail.com>
2026-03-13 10:57:26 +0000

    -

---
+++ src/features/photography/gallery.js
@@ -0,0 +1,10 @@
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+function gallery() {
+  console.log('hey')
+}
+
+export default gallery
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import footer from './features/footer'
-import gallery from './features/gallery'
+// import footer from './features/footer'
+import gallery from './features/photography/gallery'
 import headingMask from './features/heading_mask'
 import heroIntro from './features/hero_intro'
-import nav from './features/nav'
+// import nav from './features/nav'
 import videos from './features/videos'
 
 import './styles/style.css'
@@ -26,5 +26,5 @@ if (document.body.classList.contains('body__home')) runHomeFunctions()
 if (document.body.classList.contains('body__fotografia')) runGalleryFunctions()
 if (document.body.classList.contains('body__video')) runVideoFunctions()
 
-nav()
-footer()
+// nav()
+// footer()
