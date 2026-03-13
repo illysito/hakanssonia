6fd88121a171280586f9aaa5751e69ffb33fcabf
illysito <illyalukyanovdesign@gmail.com>
2026-03-13 11:40:09 +0000

    Update gallery.js

--- src/features/photography/gallery.js
+++ src/features/photography/gallery.js
@@ -6,15 +6,39 @@ gsap.registerPlugin(ScrollTrigger)
 function gallery() {
   const imgContainers = document.querySelectorAll('.img-container')
 
+  function markIfInViewport() {
+    // OBSERVER
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          entry.target.dataset.inViewport = entry.isIntersecting
+            ? 'true'
+            : 'false'
+        })
+      },
+      {
+        root: null, // viewport
+        rootMargin: '0px',
+        threshold: 0.1, // counts as visible when 10% is inside viewport
+      }
+    )
+
+    imgContainers.forEach((cont) => {
+      img.dataset.inViewport = 'false'
+      observer.observe(cont)
+    })
+  }
+  markIfInViewport()
+
   imgContainers.forEach((cont) => {
     const randomAmp = 4
     const random = randomAmp * Math.random() - randomAmp / 2
     gsap.to(cont, {
-      y: 200 + random,
+      y: 80 + random,
       scrollTrigger: {
         trigger: cont,
-        start: 'top bottom',
-        end: 'top top',
+        start: 'top 120%',
+        end: 'bottom top',
         scrub: true,
       },
     })
