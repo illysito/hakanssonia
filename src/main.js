218b4635a5bdae67f448501d29a9831b3a54b172
illysito <illyalukyanovdesign@gmail.com>
2026-03-13 11:09:42 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,30 +1,7 @@
-// import footer from './features/footer'
 import gallery from './features/photography/gallery'
-// import headingMask from './features/heading_mask'
-// import heroIntro from './features/hero_intro'
-// // import nav from './features/nav'
-// import videos from './features/videos'
 
 import './styles/style.css'
 
-console.log('Yeka!')
+console.log('Sonia 2026')
 
-// function runHomeFunctions() {
-//   heroIntro()
-//   headingMask()
-// }
-
-function runGalleryFunctions() {
-  gallery()
-}
-
-// function runVideoFunctions() {
-//   videos()
-// }
-
-// if (document.body.classList.contains('body__home')) runHomeFunctions()
-if (document.body.classList.contains('body__fotografia')) runGalleryFunctions()
-// if (document.body.classList.contains('body__video')) runVideoFunctions()
-
-// nav()
-// footer()
+gallery()
