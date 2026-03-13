636f324ea2459f5477f0448b0f1b46a59610467a
illysito <illyalukyanovdesign@gmail.com>
2026-03-13 11:35:24 +0000

    Update gallery.js

--- src/features/photography/gallery.js
+++ src/features/photography/gallery.js
@@ -4,7 +4,21 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 gsap.registerPlugin(ScrollTrigger)
 
 function gallery() {
-  console.log('hey')
+  const imgContainers = document.querySelectorAll('.img-container')
+
+  imgContainers.forEach((cont) => {
+    const randomAmp = 4
+    const random = randomAmp * Math.random() - randomAmp / 2
+    gsap.to(cont, {
+      y: -20 + random,
+      scrollTrigger: {
+        trigger: cont,
+        start: 'top bottom',
+        end: 'top top',
+        scrub: true,
+      },
+    })
+  })
 }
 
 export default gallery
